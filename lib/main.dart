b322b672419ceedcce547daa681c75e9805d203c
Nihal-Mohammed-V <nihalmhdv25@gmail.com>
2024-12-25 15:08:45 +0530

    Add firebase config files & some assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:aurakart/app.dart';
+import 'package:get/get.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+// Entry Point of Flutter App
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  ).then((FirebaseApp value) => Get.put(AuthenticationRepository()));
+
   runApp(const MyApp());
 }
-
